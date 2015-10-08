64ac3ec8f1a53945cbb7816d2b55e140a3f92833
weike <weikebi.la@gmail.com>
2015-10-07 22:19:50 -0700

    add google analyslitscs

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 Parse.initialize("ewBEqPQpVnjDmFtKyFEPnRqTcwjdoXG7QAjswiKn", "IsbN1GJKnZFUy2peWC5GXiV1GYJwivbSAh9GyknD");
 var currentUser = Parse.User.current();
+
 if (currentUser) {
     ;
 } else {
