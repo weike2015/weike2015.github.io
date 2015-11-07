473a7d8911704a6542131cc3ed195f8ee31568e2
Work <weike@Rudys-MacBook-Air.local>
2015-11-07 14:57:45 -0800

    Initial commit

--- pages/index.js
+++
@@ -1,9 +0,0 @@
-Parse.initialize("ewBEqPQpVnjDmFtKyFEPnRqTcwjdoXG7QAjswiKn", "IsbN1GJKnZFUy2peWC5GXiV1GYJwivbSAh9GyknD");
-var currentUser = Parse.User.current();
-
-if (currentUser) {
-    ;
-} else {
-    window.location.replace("login.html");
-}
-
