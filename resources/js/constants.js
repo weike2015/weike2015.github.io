9a0ae166fcbac43f1af7626df49a2f4ee2188f79
Work <weike@Rudys-MacBook-Air.local>
2015-11-07 15:44:07 -0800

    fix link

--- resources/js/constants.js
+++ resources/js/constants.js
@@ -3,8 +3,8 @@
  */
  var APP_NAME = 'alphabet_game';
  var TRACKER_URL = "//tracker.siminsights.com/Tracker/v1/track";
- var INIT_URL = "http://1106.elasticbeanstalk.com//pomdp/init";
- var SUBMIT_URL = "http://1106.elasticbeanstalk.com//pomdp/submitAnswer";
+ var INIT_URL = "http://1106.elasticbeanstalk.com/pomdp/init";
+ var SUBMIT_URL = "http://1106.elasticbeanstalk.com/pomdp/submitAnswer";
  
  // data logging codes
  var ACTION_STARTED = 1010,
